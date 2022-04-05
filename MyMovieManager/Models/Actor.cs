bcf8ec944cdc4f99eb7f1a294bee71166036b45b
Gerson Azabache <gerson_dunk@hotmail.com>
2022-04-05 18:49:23 -0500

    creating first models}

---
+++ MyMovieManager/Models/Actor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMovieManager.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [MaxLength(50,ErrorMessage = "No se puede exceder de 50 caracteres")]
+        [Required(ErrorMessage = "El nombre del actor es obligatorio")]        
+        [Display(Name = "Nombres del actor")]
+        public string Nombres { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [MaxLength(50, ErrorMessage = "No se puede exceder de 50 caracteres")]
+        [Required(ErrorMessage = "El apellido del actor es obligatorio")]
+        [Display(Name = "Apellidos del actor")]
+        public string Apellidos { get; set; }
+
+        [Display(Name = "Edad del actor")]
+        public int Edad { get; set; }
+    }
+}
---
+++ MyMovieManager/Models/Genero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMovieManager.Models
+{
+    public class Genero
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required(ErrorMessage = "El campo nombre es obligatorio")]
+        [Display(Name = "Nombre del género")]
+        [MaxLength(50,ErrorMessage = "El nombre no debe exceder de 50 caracteres")]
+        public string Nombre { get; set; }
+    }
+}
