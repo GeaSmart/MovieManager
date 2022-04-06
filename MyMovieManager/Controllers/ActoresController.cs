9e30e25f192ce5aa57d0cd0d159c4f294839e543
Gerson Azabache <gerson_dunk@hotmail.com>
2022-04-06 15:48:43 -0500

    actores simple crud done

--- MyMovieManager/Controllers/ActoresController.cs
+++ MyMovieManager/Controllers/ActoresController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MyMovieManager.Helpers.MisGastos.Helpers;
 using MyMovieManager.Models;
 using System;
 using System.Collections.Generic;
@@ -34,5 +35,37 @@ namespace MyMovieManager.Controllers
             return View(actor);
         }
 
+        [HttpPost]
+        public async Task<ActionResult> AddOrEdit(int id, [FromForm] Actor actor)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id == 0) //insert
+                {
+                    context.Actores.Add(actor);
+                    await context.SaveChangesAsync();
+                }
+                else //update
+                {
+                    context.Actores.Update(actor);
+                    await context.SaveChangesAsync();
+                }
+                return Json(new { isValid = true, html = RenderRazor.RenderRazorViewToString(this, "_ViewAll", context.Actores.ToList()) });
+            }
+            return Json(new { isValid = false, html = RenderRazor.RenderRazorViewToString(this, "AddOrEdit", actor) });
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var actor = await context.Actores.FirstOrDefaultAsync(x => x.Id == id);
+            if (actor == null)
+                return NotFound();
+
+            context.Actores.Remove(actor);
+            await context.SaveChangesAsync();
+
+            return Json(new { isValid = true, html = RenderRazor.RenderRazorViewToString(this, "_ViewAll", context.Actores.ToList()) });
+        }
     }
 }
