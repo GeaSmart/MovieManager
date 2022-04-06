df1c600802a8e606f395a08c0d5d5de2c65c91e2
Gerson Azabache <gerson_dunk@hotmail.com>
2022-04-05 19:52:15 -0500

    first modal

---
+++ MyMovieManager/Controllers/GenerosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyMovieManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMovieManager.Controllers
+{
+    public class GenerosController:Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public GenerosController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var generos = await context.Generos.ToListAsync();
+            return View(generos);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> AddOrEdit(int id = 0)
+        {
+            var genero = await context.Generos.FirstOrDefaultAsync(x=>x.Id == id);
+
+            if (id == 0)
+                return View(new Genero());
+
+            return View(genero);
+        }
+    }
+}
--- MyMovieManager/wwwroot/js/site.js
+++ MyMovieManager/wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function showModal(url, title) {
+    $.ajax(
+        {
+            type: "GET",
+            url: url,
+            success: function (response) {
+                $("#form-modal .modal-body").html(response);
+                $("#form-modal .modal-title").html(title);
+                $("#form-modal").modal('show');
+            }
+        }
+    )
+}
