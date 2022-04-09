067f7c5751088c81ef5c7883a6964079286ff7af
Gerson Azabache <gerson_dunk@hotmail.com>
2022-04-09 13:58:02 -0500

    adding peliculas controller

---
+++ MyMovieManager/Controllers/PeliculasController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMovieManager.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public PeliculasController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var peliculas = await context.Peliculas.ToListAsync();
+            return View(peliculas);
+        }
+
+    }
+}
